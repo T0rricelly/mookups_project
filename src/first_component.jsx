feb15b069d2092315dcf3040137bf3470cc9e6ec
Kevin <kevin@example.com>
2025-03-23 17:01:52 -0500

    Haciendo mi primer componente

---
+++ src/first_component.jsx
@@ -0,0 +1,11 @@
+function First_Component() {
+    return (
+        <>
+        <h1>Hola mundo</h1>
+        <ol>Hola</ol>
+        <ol>Hola</ol>
+        </>
+    );
+}
+
+export default First_Component;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,8 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import First_Component from './first_component'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <>
+    <First_Component/>
+  </>
 )
